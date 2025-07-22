a818fcc1a1f397e82e982cfb59625b00668b2808
mertensblackfyre <72759681+mertensblackfyre@users.noreply.github.com.>
2025-07-22 20:54:05 +0300

    Added Helper Class

--- main.cc
+++ main.cc
@@ -1,11 +1,16 @@
 #include <iostream>
 
-
-
-
+#include <spdlog/spdlog.h>
 
 int main(int argc, char *argv[]) {
 
+  if (argc > 3) {
+    spdlog::error("[Usage]: transpicta [option] [directory]");
+    return -1;
+  };
+  std::string d_name = argv[2];
+  std::string option = argv[1];
 
-
+  if (argv[1] == option) {
+  };
 };
